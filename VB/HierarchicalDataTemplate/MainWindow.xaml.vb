cb269ba2d490573308d3757fed01cf00bff43cc8
DevExpressExampleBot <fakeEmail>
2021-08-11 17:50:50 +0300

    Source auto update [skip ci]

--- VB/HierarchicalDataTemplate/MainWindow.xaml.vb
+++ VB/HierarchicalDataTemplate/MainWindow.xaml.vb
@@ -2,14 +2,14 @@
 
 
 Namespace HierarchicalDataTemplate
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits DXWindow
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits DXWindow
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
 End Namespace
--- VB/HierarchicalDataTemplate/ViewModel.vb
+++ VB/HierarchicalDataTemplate/ViewModel.vb
@@ -1,50 +1,50 @@
 ﻿Imports System.Collections.Generic
-Imports System.Collections.ObjectModel;
+Imports System.Collections.ObjectModel
 
 Namespace HierarchicalDataTemplate
 
-    Public Class ViewModel
-        Public Property MyData() As Data
-        Public Property SelectedItem() As Object
-        Public Sub New()
-            MyData = New Data()
-        End Sub
-    End Class
-    Public Class Data
-        Public Property Categories() As ObservableCollection(Of Category)
-        Public Sub New()
-            Categories = New ObservableCollection(Of Category)()
-            Dim subitems As New ObservableCollection(Of Item)()
-            subitems.Add(New Item() With { _
-                .ItemName = "Chair", _
-                .Description = "A red chair." _
-            })
-            subitems.Add(New Item() With { _
-                .ItemName = "Table", _
-                .Description = "An old table." _
-            })
-            Categories.Add(New Category() With { _
-                .CategoryName = "Furniture", _
-                .Items = subitems _
-            })
-            Dim books As New ObservableCollection(Of Item)()
-            books.Add(New Item() With { _
-                .ItemName = "Dictionary", _
-                .Description = "My old French-English Dictionary" _
-            })
-            Categories.Add(New Category() With { _
-                .CategoryName = "Books", _
-                .Items = books _
-            })
-        End Sub
-    End Class
-    Public Class Category
-        Public Property CategoryName() As String
-        Public Property Description() As String
-        Public Property Items() As ObservableCollection(Of Item)
-    End Class
-    Public Class Item
-        Public Property ItemName() As String
-        Public Property Description() As String
-    End Class
+	Public Class ViewModel
+		Public Property MyData() As Data
+		Public Property SelectedItem() As Object
+		Public Sub New()
+			MyData = New Data()
+		End Sub
+	End Class
+	Public Class Data
+		Public Property Categories() As ObservableCollection(Of Category)
+		Public Sub New()
+			Categories = New ObservableCollection(Of Category)()
+			Dim subitems As New ObservableCollection(Of Item)()
+			subitems.Add(New Item() With {
+				.ItemName = "Chair",
+				.Description = "A red chair."
+			})
+			subitems.Add(New Item() With {
+				.ItemName = "Table",
+				.Description = "An old table."
+			})
+			Categories.Add(New Category() With {
+				.CategoryName = "Furniture",
+				.Items = subitems
+			})
+			Dim books As New ObservableCollection(Of Item)()
+			books.Add(New Item() With {
+				.ItemName = "Dictionary",
+				.Description = "My old French-English Dictionary"
+			})
+			Categories.Add(New Category() With {
+				.CategoryName = "Books",
+				.Items = books
+			})
+		End Sub
+	End Class
+	Public Class Category
+		Public Property CategoryName() As String
+		Public Property Description() As String
+		Public Property Items() As ObservableCollection(Of Item)
+	End Class
+	Public Class Item
+		Public Property ItemName() As String
+		Public Property Description() As String
+	End Class
 End Namespace
