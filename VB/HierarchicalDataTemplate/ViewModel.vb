e2271ca05ed2cbdbbf330f910fe4c20bd3f142ac
Anthony Sam <anton.sameishchev@devexpress.com>
2021-08-11 17:45:59 +0300

    Update ViewModel.vb

--- VB/HierarchicalDataTemplate/ViewModel.vb
+++ VB/HierarchicalDataTemplate/ViewModel.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Collections.Generic
+Imports System.Collections.ObjectModel;
 
 Namespace HierarchicalDataTemplate
 
@@ -10,10 +11,10 @@ Namespace HierarchicalDataTemplate
         End Sub
     End Class
     Public Class Data
-        Public Property Categories() As List(Of Category)
+        Public Property Categories() As ObservableCollection(Of Category)
         Public Sub New()
-            Categories = New List(Of Category)()
-            Dim subitems As New List(Of Item)()
+            Categories = New ObservableCollection(Of Category)()
+            Dim subitems As New ObservableCollection(Of Item)()
             subitems.Add(New Item() With { _
                 .ItemName = "Chair", _
                 .Description = "A red chair." _
@@ -26,7 +27,7 @@ Namespace HierarchicalDataTemplate
                 .CategoryName = "Furniture", _
                 .Items = subitems _
             })
-            Dim books As New List(Of Item)()
+            Dim books As New ObservableCollection(Of Item)()
             books.Add(New Item() With { _
                 .ItemName = "Dictionary", _
                 .Description = "My old French-English Dictionary" _
@@ -40,7 +41,7 @@ Namespace HierarchicalDataTemplate
     Public Class Category
         Public Property CategoryName() As String
         Public Property Description() As String
-        Public Property Items() As List(Of Item)
+        Public Property Items() As ObservableCollection(Of Item)
     End Class
     Public Class Item
         Public Property ItemName() As String
